19cc19acb53fd8cd787f2e377b688ae3ac193d88
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2024-09-13 00:22:30 +0100

    🚧 importing remotes dynamically

--- src/core/RemoteModuleLoader.ts
+++ src/core/RemoteModuleLoader.ts
@@ -1,13 +1,27 @@
 
 import { RemoteModule } from 'composaic/lib/core/init';
 
+// https://www.npmjs.com/package/module-federation-import-remote
+
 /**
  * Loads a remote module.
  * @param remoteModule - The remote module to load.
  */
 export const loadRemoteModule = async (
     remoteModule: RemoteModule
-): Promise<object | undefined> => {
+): Promise<unknown | undefined> => {
     const { url, name, bundleFile, moduleName } = remoteModule;
-    return undefined;
+
+    // url + bundleFile = full path to the bundle file
+    // scope = name
+    // module = moduleName
+    const scope = name;
+
+    await __webpack_init_sharing__("default");
+    const container = (window as Window)[scope] as WebPackContainer; // or get the container somewhere else
+    // Initialize the container, it may provide shared modules
+    await container.init(__webpack_share_scopes__.default);
+    const factory = await ((window as Window)[scope] as WebPackContainer).get(moduleName);
+    const Module = factory();
+    return Module;
 };
--- src/declaration.d.ts
+++ src/declaration.d.ts
@@ -2,3 +2,14 @@ declare module '*.scss' {
     const content: { [className: string]: string };
     export default content;
 }
+
+declare const __webpack_init_sharing__: (scope: string) => void;
+
+declare interface WebPackContainer {
+    init: (shareScope: string) => Promise<void>;
+    get: (module: string) => Promise<() => unknown>;
+}
+
+declare interface Window { [key: string]: unknown };
+
+declare const __webpack_share_scopes__: { default: string };
